c00c3352f1341cf4e2a64a06582b8a1b3d9b4c67
Spencer-Sch <spencer.schoeneman@gmail.com>
2021-01-31 14:18:42 -0600

    updated JS file
    added answers for lessons 10 & 11

--- FCC_data-structures.js
+++ FCC_data-structures.js
@@ -92,3 +92,33 @@ function spreadOut() {
 
 console.log(spreadOut());
 
+// Last Commit
+// Lesson 10
+
+function quickCheck(arr, elem) {
+  // Only change code below this line
+  if (arr.indexOf(elem) >= 0) {
+    return true;
+  } else {
+    return false;
+  }
+  // Only change code above this line
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+// Lesson 11
+
+function filteredArray(arr, elem) {
+  let newArr = [];
+  // Only change code below this line
+  for (let i=0; i<arr.length; i++) {
+    if (arr[i].indexOf(elem) < 0 ) {
+      newArr.push(arr[i]);
+    }
+  }
+  // Only change code above this line
+  return newArr;
+}
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
