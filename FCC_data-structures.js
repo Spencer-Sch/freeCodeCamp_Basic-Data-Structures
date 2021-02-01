9d47608252b6e9f5e931473a5b1184d132912792
Spencer-Sch <spencer.schoeneman@gmail.com>
2021-02-01 15:51:39 -0600

    Update to JS file
    Addition of final lesson answers for the section.

--- FCC_data-structures.js
+++ FCC_data-structures.js
@@ -92,7 +92,6 @@ function spreadOut() {
 
 console.log(spreadOut());
 
-// Last Commit
 // Lesson 10
 
 function quickCheck(arr, elem) {
@@ -262,4 +261,64 @@ function countOnline(usersObj) {
   // Only change code above this line
 }
 
-// Last Commit
+
+// Lesson 19
+
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  // Only change code below this line
+  let usersArr = Object.keys(obj);
+  return usersArr;
+  // Only change code above this line
+}
+
+console.log(getArrayOfUsers(users));
+
+// Lesson 20
+
+let user = {
+  name: 'Kenneth',
+  age: 28,
+  data: {
+    username: 'kennethCodesAllDay',
+    joinDate: 'March 26, 2016',
+    organization: 'freeCodeCamp',
+    friends: [
+      'Sam',
+      'Kira',
+      'Tomo'
+    ],
+    location: {
+      city: 'San Francisco',
+      state: 'CA',
+      country: 'USA'
+    }
+  }
+};
+
+function addFriend(userObj, friend) {
+  // Only change code below this line
+  userObj.data.friends.push(friend);
+  return userObj.data.friends;
+  // Only change code above this line
+}
+
+console.log(addFriend(user, 'Pete'));
